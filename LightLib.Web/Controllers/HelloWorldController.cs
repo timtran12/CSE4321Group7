8eb8d803b7dd5be621aff07a04b6fbed5742dca5
Silvision <richardmnguyen@hotmail.com>
2022-04-17 00:54:55 -0500

    changed index.html contents to book and started POST request

---
+++ LightLib.Web/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LightLib.Web.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+
+        }
+
+        public string Welcome()
+        {
+
+            return "This is welcome action method...";
+        }
+
+    }
+}
